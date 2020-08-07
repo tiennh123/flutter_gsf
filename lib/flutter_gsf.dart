a451aa130b5fd19155d8c22fa4deaa1fdbfbb5be
tien.nh <tien.nh@mekongpetro.com>
2020-08-07 12:44:57 +0700

    save

--- lib/flutter_gsf.dart
+++ lib/flutter_gsf.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class FlutterGsf {
-  static const MethodChannel _channel =
-      const MethodChannel('flutter_gsf');
+  static const MethodChannel _channel = const MethodChannel('flutter_gsf');
 
   static Future<String> get getGsf async {
     final String version = await _channel.invokeMethod('getGsf');
