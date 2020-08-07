ed29c10dee422780cdc38cd7a9232ec376014da8
tien.nh <tien.nh@mekongpetro.com>
2020-08-07 14:07:05 +0700

    save

--- lib/flutter_gsf.dart
+++ lib/flutter_gsf.dart
@@ -6,7 +6,8 @@ class FlutterGsf {
   static const MethodChannel _channel = const MethodChannel('flutter_gsf');
 
   static Future<String> get getGsf async {
-    final String version = await _channel.invokeMethod('getGsf');
+    final data = await _channel.invokeMethod('getGsf');
+    final String version = data != null ? data : "";
     return version;
   }
 }
